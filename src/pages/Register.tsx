ba8a980de511db44557476e59318a53af0d923c1
GSaber <saber.guenaoui@gmail.com>
2023-08-30 13:31:53 +0100

    cleaning the react-app

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">CHAT-APP</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Display name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="password" />
+          <input type="file" placeholder="Avatar" />
+          <button>Sign up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
 };
