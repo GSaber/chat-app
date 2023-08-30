e87894a459cb3ebd3fa8e33a8de417cc71bf8935
GSaber <saber.guenaoui@gmail.com>
2023-08-30 18:13:35 +0100

    clean app & index tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import "./App.css";
-import { Register } from "./pages/Register";
+import "./style.scss";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Home />
     </div>
   );
 }
