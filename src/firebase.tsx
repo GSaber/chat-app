a29ed804dfb589d00a18407aff87822fcce94d01
GSaber <saber.guenaoui@gmail.com>
2023-08-31 09:45:20 +0100

    add firebase storage

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,6 +16,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 //Call authentification function
-const auth = getAuth();
+export const auth = getAuth();
+// Create a root reference
+export const storage = getStorage();
