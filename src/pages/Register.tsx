3732efd45f6f609bfaef3c2517b634b7b115ba22
GSaber <saber.guenaoui@gmail.com>
2023-08-31 09:46:15 +0100

    add register to firebase

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
+
 function Register() {
+  const [err, setErr] = useState(false);
+  const handleSubmit = async (e: {
+    preventDefault: () => void;
+    target: any;
+  }) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        "state_changed",
+
+        (error) => {
+          // Handle unsuccessful uploads
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">CHAT-APP</span>
         <span className="title">Register</span>
-        <form>
-          <input type="text" placeholder="Display name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="password" />
-          <input style={{ display: "none" }} type="file" id="avatar" />
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Display name" onChange={(e) => {}} />
+          <input type="email" placeholder="Email" onChange={(e) => {}} />
+          <input type="password" placeholder="password" onChange={(e) => {}} />
+          <input
+            style={{ display: "none" }}
+            type="file"
+            id="avatar"
+            onChange={(e) => {}}
+          />
           <label htmlFor="avatar">
             <img src={require("../img/AddAvatar.png")} alt="" />
             <span>Add Avatar</span>
           </label>
           <button>Sign up</button>
+          {err && <span>Something went wrong</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
