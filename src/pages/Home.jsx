5b576886688a7c5c06c11f57cb399eaeac4963de
GSaber <saber.guenaoui@gmail.com>
2023-09-06 15:26:27 +0100

    cleaning file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import Sidebar from "../components/Sidebar";
 import Chat from "../components/Chat";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-import { Navigate } from "react-router-dom";
 function Home() {
   const currentUser = useContext(AuthContext);
 
