3cf6bb3c4eb7e1e9f35982a2b6633b0a3e333fec
GSaber <saber.guenaoui@gmail.com>
2023-09-06 15:27:01 +0100

    add search user query

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,20 +1,48 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 const Searchbar = () => {
+  const [username, setUserName] = useState("");
+  const [user, setUser] = useState();
+  const [err, setErr] = useState(false);
+  const handleSearch = async () => {
+    setUser(null);
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        setUser(doc.data());
+      });
+    } catch (error) {
+      setErr(true);
+    }
+  };
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="text" placeholder="Find a user" />
-      </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/15242091/pexels-photo-15242091/free-photo-of-gens-centrale-feuilles-debout.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
+        <input
+          type="text"
+          placeholder="Find a user"
+          onKeyDown={handleKey}
+          onChange={(e) => setUserName(e.target.value)}
         />
-        <div className="userChatInfo">
-          <span>Saber</span>
-        </div>
       </div>
+      {err && <span>User not found</span>}
+      {user && (
+        <div className="userChat">
+          <img src={user?.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user?.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
